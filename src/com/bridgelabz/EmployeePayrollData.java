8521b306cec02cda5c8d289e40efe032cd77b151
shravkv <sravan.k.vodnala@gmail.com>
2022-07-13 22:23:46 +0530

    Added: Create employee payroll class of id,name and salary

---
+++ src/com/bridgelabz/EmployeePayrollData.java
@@ -0,0 +1,22 @@
+package com.bridgelabz;
+
+public class EmployeePayrollData {
+    int id;
+    String name;
+    double salary;
+
+    public EmployeePayrollData(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "EmployeePayrollData{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
--- src/com/bridgelabz/EmployeePayrollService.java
+++ src/com/bridgelabz/EmployeePayrollService.java
@@ -1,8 +1,40 @@
 package com.bridgelabz;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class EmployeePayrollService {
+    private List<EmployeePayrollData> employeePayrollList;
+
+    public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList) {
+        this.employeePayrollList = employeePayrollList;
+    }
+
+    public EmployeePayrollService() {
+    }
 
     public static void main(String[] args) {
-        System.out.println("Welcome To Employee Payroll Problem");
+        System.out.println("Welcome To Employee Payroll Service");
+        ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
+        Scanner scanner = new Scanner(System.in);
+        employeePayrollService.readEmployeePayRollData(scanner);
+        employeePayrollService.writeEmployeePayrollData();
+    }
+
+    public void readEmployeePayRollData(Scanner scanner) {
+        System.out.println("Enter employee id");
+        int id = scanner.nextInt();
+        System.out.println("Enter employee name");
+        String name = scanner.next();
+        System.out.println("Employee salary");
+        double salary = scanner.nextDouble();
+        employeePayrollList.add(new EmployeePayrollData(id, name, salary));
+
+    }
+
+    public void writeEmployeePayrollData() {
+        System.out.println("\n writing Employee Payroll Roaster to console \n" + employeePayrollList);
     }
 }
